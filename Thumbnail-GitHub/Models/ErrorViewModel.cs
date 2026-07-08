5cfa577cd631dc33c5402aaa1ca5a834dfe9d2ee
wa0102 <minon.maro@gmail.com>
2026-07-08 15:00:34 +0900

    Delete Thumbnail-GitHub directory

--- Thumbnail-GitHub/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Thumbnail.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
